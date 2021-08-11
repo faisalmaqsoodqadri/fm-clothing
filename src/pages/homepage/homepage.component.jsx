649143bc48d7df80ba91df2f86a5e9ff15c409c1
Faisal Maqsood <fmqswift@yahoo.com>
2021-08-11 08:57:20 +0300

    Create Home page menu-item style

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.compnent'
+import MenuItem from '../../components/menu-item/menu-item.component'
+
+
+const Homepage = () => (
+
+    <div className='homepage'>
+
+
+            <Directory />
+           
+
+       
+    </div>
+
+
+);
+
+export default Homepage;
