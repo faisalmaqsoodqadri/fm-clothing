773559512adbe9db6aed0c936280ef756b321009
Faisal Maqsood <fmqswift@yahoo.com>
2021-08-25 10:08:29 +0300

    Add Header and Links to Pages in Menu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import {Switch, Route } from 'react-router-dom'
 import ShopPage from './components/shop/shop.component';
-
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      
+      <Header />
+
+
      <Switch>
       <Route exact path = '/' component={ Homepage } />
         <Route path='/shop' component={ShopPage} />
